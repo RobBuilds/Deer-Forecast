8fc9dec0d9cc6f99ee7d1c6d3e4ee4c5a055d9f7
RobBuilds <152198994+RobBuilds@users.noreply.github.com>
2025-08-05 12:25:03 -0400

    force redeploy

--- deer-forecast/src/app/searchbar.tsx
+++ deer-forecast/src/app/searchbar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, {useState, useEffect} from 'react';
-
+//trigger redeploy
 //import {OpenAI} from "openai";
 export default function SearchBar(){
     const [latQuery, setLatQuery] = useState("");
